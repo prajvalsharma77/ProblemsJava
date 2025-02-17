f4e6f07f05108733873ffc3d86d92b459fc6c8b0
prajvalsharma <prajvalsharma3375@gmail.com>
2025-02-17 22:11:09 +0530

    Add files via upload

---
+++ Pyramid2.java
@@ -0,0 +1,17 @@
+public class Pyramid2 {
+    public static void main(String[] args) {
+        int rows = 10; // Directly setting the number of rows
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = i; j < rows; j++) {
+System.out.println("");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to next line
+        }
+    }
+}
