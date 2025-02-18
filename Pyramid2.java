e6822732c0344808ceac314be835e320c037710c
prajvalsharma <prajvalsharma3375@gmail.com>
2025-02-18 23:32:21 +0530

    Update Pyramid2.java

--- Pyramid2.java
+++ Pyramid2.java
@@ -3,10 +3,10 @@ public class Pyramid2 {
         int rows = 10; // Directly setting the number of rows
 
         for (int i = 1; i <= rows; i++) {
-            // Print spaces
+            // Print Various rows "the outer loop"
             for (int j = i; j < rows; j++) {
 System.out.println("");
-            }
+            } // print the spaces between stars properly inner loop
             // Print stars
             for (int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("*");
